a546ad84c6b9342860d3e7aae3134b5029bb6afd
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-07-24 13:59:05 -0300

    First Commit

---
+++ src/main/java/br/com/antonio/calcmeback/CalcmeBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.antonio.calcmeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcmeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/antonio/calcmeback/CalcmeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antonio.calcmeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
